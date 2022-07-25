245e1027876926e4b2680cb397e1439d7d72af73
Ebrahim Ahmed <87176403+MrEbrahimXD@users.noreply.github.com>
2022-07-26 00:23:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 
-
 function rfAudio() {
     let x = Math.round(Math.random());
 
     if (x == 1) {
         document.getElementById('firstAudio').play();
-        document.getElementById('firstAudio').addEventListener('ended',function(){
-            alert('You found the first magic lol ! ');
+        document.getElementById('firstAudio').addEventListener('ended',() => {
+            //
+
         });
     } else {
         document.getElementById('secAudio').play();
-        document.getElementById('secAudio').addEventListener('ended',function() {
-            alert('You found the second magic lol !!!! congrats !');
+        document.getElementById('secAudio').addEventListener('ended' , () => {
+            //
+        
         });
     } 
     
 }
+
+
+$(document).ready( () => {
+    $(".nav-button").click( () => {
+      $(this).toggleClass("open")
+      $('.nav-ul').toggleClass("open");
+    });
+  });
+  
 
 
 
