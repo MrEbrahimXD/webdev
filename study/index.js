32f9c5c8a4067a5c13dcfdd878ed7e9c0be6267c
Ebrahim Ahmed <87176403+MrEbrahimXD@users.noreply.github.com>
2022-08-19 14:12:16 +0200

    Delete study directory

--- study/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready( function() {
-    $(".nav-button").click( function() {
-      $(this).toggleClass("open");
-      $('.ul').toggleClass("open");
-      $('.nav-ul').toggleClass("open");
-    });
-  });
