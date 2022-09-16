9be9edd08ead079549976cb4c84e1c086fba9103
Ebrahim Ahmed <87176403+MrEbrahimXD@users.noreply.github.com>
2022-09-16 06:14:40 +0200

    Add files via upload

---
+++ aya/writer.js
@@ -0,0 +1,16 @@
+function typeEffect(element, speed) {
+	var text = element.innerHTML;
+	element.innerHTML = "";
+	
+	var i = 0;
+	var timer = setInterval(function() {
+    if (i < text.length) {
+      element.append(text.charAt(i));
+      i++;
+    } else {
+      clearInterval(timer);
+    }
+  }, speed);
+}
+
+
